db1e7ac57c3157f9f40fc88b39139f2cae9839b1
Jonathan Felix <jhow.wi56@gmail.com>
2021-11-15 13:11:47 -0300

    responseEntity

--- src/main/java/com/devsuperior/workshopmongo/resources/UserResource.java
+++ src/main/java/com/devsuperior/workshopmongo/resources/UserResource.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,12 +18,12 @@ public class UserResource {
 	
 	
 	@GetMapping
-	public List<User> findAll(){
+	public ResponseEntity<List<User>> findAll(){
 		User maria = new User("1", "Maria Bronw", "maria@gmail.com");
 		User alex = new User("2", "Alex Green", "alex@gmail.com");
 		List<User> list = new ArrayList<>();
 		list.addAll(Arrays.asList(maria, alex));
-		return list;
+		return ResponseEntity.ok().body(list);
 		
 	}
 
