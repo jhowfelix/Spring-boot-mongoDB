ca8de495c1b3edb4f0237cfac43cc98f0b8f4de9
Jonathan Felix <jhow.wi56@gmail.com>
2021-11-21 13:23:39 -0300

    referenciando os posts do usuario

--- src/main/java/com/devsuperior/workshopmongo/config/Instantiation.java
+++ src/main/java/com/devsuperior/workshopmongo/config/Instantiation.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.devsuperior.workshopmongo.domain.Post;
 import com.devsuperior.workshopmongo.domain.User;
+import com.devsuperior.workshopmongo.dto.AuthorDTO;
 import com.devsuperior.workshopmongo.repositories.PostRepository;
 import com.devsuperior.workshopmongo.repositories.UserRepository;
 
@@ -31,14 +32,15 @@ public class Instantiation implements CommandLineRunner {
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		userRepository.saveAll(Arrays.asList(maria,alex,bob));
 		
 
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "partiu viagem", "vou viajar para São Paulo. Abraços.", maria);
-		Post post2 = new Post(null, sdf.parse("21/03/2018"), "partiu viagem", "vou viajar para São Paulo. Abraços.", maria);
-		userRepository.saveAll(Arrays.asList(maria,alex,bob));
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "partiu viagem", "vou viajar para São Paulo. Abraços.", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("21/03/2018"), "partiu viagem", "vou viajar para São Paulo. Abraços.", new AuthorDTO(maria));
 		postRepository.saveAll(Arrays.asList(post1,post2));
 		
-		
+		maria.getPost().addAll(Arrays.asList(post1, post2));
+		userRepository.save(maria);
 	}
 
 }
